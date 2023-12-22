2b7ec3f17f81f8421810d162aa247208c926ef6f
akshaykumarbedre <akshaykumarbedre.bm@gmail.com>
2023-12-22 16:01:35 +0530

    Training_pipeline completed test

--- src/pipelines/prediction_pipeline.py
+++ src/pipelines/prediction_pipeline.py
@@ -11,6 +11,7 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
+            #feature #['national_inv', 'lead_time', 'in_transit_qty', 'forecast_3_month', 'sales_1_month', 'min_bank', 'perf_6_month_avg', 'went_on_backorder']
             preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
             model_path=os.path.join('artifacts','model.pkl')
 
@@ -27,36 +28,38 @@ class PredictPipeline:
             logging.info("Exception occured in prediction")
             raise CustomException(e,sys)
         
-'''class CustomData:
-    def __init__(self,carat:float,depth:float,table:float,x:float,y:float,z:float,cut:str,color:str,clarity:str):        
-        self.carat=carat
-        self.depth=depth
-        self.table=table
-        self.x=x
-        self.y=y
-        self.z=z
-        self.cut = cut
-        self.color = color
-        self.clarity = clarity
+class CustomData:
+    def __init__(self,national_inv:float,lead_time:float,in_transit_qty:float,forecast_3_month:float,sales_1_month:float,min_bank:float,perf_6_month_avg:float):        
+        self.national_inv=national_inv
+        self.lead_time=lead_time
+        self.in_transit_qty=in_transit_qty
+        self.forecast_3_month=forecast_3_month
+        self.sales_1_month=sales_1_month
+        self.min_bank=min_bank
+        self.perf_6_month_avg = perf_6_month_avg
+
 
     def get_data_as_dataframe(self):
         try:
             custom_data_input_dict = {
-                'carat':[self.carat],
-                'depth':[self.depth],
-                'table':[self.table],
-                'x':[self.x],
-                'y':[self.y],
-                'z':[self.z],
-                'cut':[self.cut],
-                'color':[self.color],
-                'clarity':[self.clarity]
+            "national_inv":[self.national_inv],
+            "lead_time":[self.lead_time],
+            "in_transit_qty":[self.in_transit_qty],
+            "forecast_3_month":[self.forecast_3_month],
+            "sales_1_month":[self.sales_1_month],
+            "min_bank":[self.min_bank],
+            "perf_6_month_avg":[self.perf_6_month_avg]
             }
+
             df = pd.DataFrame(custom_data_input_dict)
             logging.info('Dataframe Gathered')
             return df
         except Exception as e:
             logging.info('Exception Occured in prediction pipeline')
             raise CustomException(e,sys)
-'''
 
+#cd=CustomData(national_inv=24,lead_time=8,in_transit_qty=0,forecast_3_month=3456,sales_1_month=10,min_bank=7,perf_6_month_avg=1)
+#df=cd.get_data_as_dataframe()
+#
+#pp=PredictPipeline()
+#print(pp.predict(df))
